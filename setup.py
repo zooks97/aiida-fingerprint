82db73727e0c92f5c17928a28cca373644c04299
Austin Zadoks <zooks97@gmail.com>
2018-07-02 15:38:23 +0200

    Fixed naming, setup setup.py and __init__.py poorly

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='aiida-fingerprint',
+      version='1.0',
+      description='Structure fingerprinting workflows for AiiDA',
+      author='Austin Zadoks',
+      author_email='austin.zadoks@epfl.ch',
+      url='https://github.com/zooks97/aiida-fingerprint',
+      packages=['aiida-fingerprint',],
+     )
