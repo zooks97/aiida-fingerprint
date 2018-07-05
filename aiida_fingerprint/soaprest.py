cafd0ff21ed16b4985e7e225bb16447eb08b97c0
Austin Zadoks <zooks97@gmail.com>
2018-07-05 15:14:05 +0200

    Modified soaprest for use with new dockerfile build

--- aiida_fingerprint/soaprest.py
+++ aiida_fingerprint/soaprest.py
@@ -64,10 +64,12 @@ class get_Soaps_v1(flask_restful.Resource):
         for argument_name in argument_names:
             parser.add_argument(ARGUMENTS[argument_name])
         args = parser.parse_args(strict=True)
-        args['atoms'] = [atoms_utils.from_dict(atoms) for atoms in json.loads(args['atoms'])]
+        args['atoms'] = [atoms_utils.from_dict(
+            atoms) for atoms in json.loads(args['atoms'])]
         args['atoms'] = [ase2qp(atom) for atom in args['atoms']]
         soaps = get_Soaps(**args)
-        soaps = [{key: value.tolist() for key, value in soap.iteritems()} for soap in soaps]
+        soaps = [{key: value.tolist() for key, value in soap.iteritems()}
+                 for soap in soaps]
 
         return flask.jsonify(soaps)
 
@@ -75,4 +77,4 @@ class get_Soaps_v1(flask_restful.Resource):
 api.add_resource(get_Soaps_v1, '/v{}/get_Soaps/'.format(VERSION))
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8899, host='0.0.0.0')
+    app.run(debug=True, port=5005, host='0.0.0.0')
